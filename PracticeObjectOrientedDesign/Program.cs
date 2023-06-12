92ed99554759fb581f8a1c7387fceb5c8b9c4b16
whitepotato <imipu.stone@gmail.com>
2023-06-12 22:57:18 +0900

    first commit

---
+++ PracticeObjectOrientedDesign/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeObjectOrientedDesign
+{
+    enum ShapeType
+    {
+        Circle,
+        Square
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Shape
+            {
+                Type = ShapeType.Circle,
+                Radius = 5
+            };
+
+            Shape square = new Shape
+            {
+                Type = ShapeType.Square,
+                SideLength = 10
+            };
+
+            double circleArea = circle.CalculateArea();
+            double squareArea = square.CalculateArea();
+
+            Console.WriteLine("円の面積: " + circleArea);
+            Console.WriteLine("正方形の面積: " + squareArea);
+        }
+    }
+}
---
+++ PracticeObjectOrientedDesign/Shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeObjectOrientedDesign
+{
+    class Shape
+    {
+        public ShapeType Type { get; set; }
+        public double Radius { get; set; }
+        public double SideLength { get; set; }
+        // ... 他の共通のプロパティやメソッド ...
+
+        public double CalculateArea()
+        {
+            double area = 0;
+
+            if (Type == ShapeType.Circle)
+            {
+                area = Math.PI * Radius * Radius;
+            }
+            else if (Type == ShapeType.Square)
+            {
+                area = SideLength * SideLength;
+            }
+            return area;
+        }
+    }
+}
